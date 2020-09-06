f14f43637f0f7a11c651ef279c2a4e4e3311dcd3
Dugbatey Blossom <Blossom@Dugbateys-MacBook-Pro.local>
2020-09-06 21:47:02 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+function myFunction() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "20px"; 
+    x.style.color = "orangered"; 
+}
+</script>
+
+<button onclick="myFunction()">Press</button>
